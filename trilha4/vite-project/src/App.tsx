ad1d4a0757fc7d46a0cb2952ebec8624a1d5f0e2
koda012 <lucaskojimaeda@gmail.com>
2025-04-15 22:43:49 -0300

    Trilha4

--- trilha4/vite-project/src/App.tsx
+++ trilha4/vite-project/src/App.tsx
@@ -8,9 +8,6 @@ import { CriarUsuario } from './components/CriarUsuario';
 const Home = () => <h1>Home</h1>;
 const About = () => <h1>About</h1>;
 const Contact = () => <h1>Contact</h1>;
-const Rooms = () => <h1>Rooms</h1>;
-const RoomDetails = () => <h1>Room Details</h1>;
-const BookingSuccess = () => <h1>Booking Success</h1>;
 
 export function App() {
   return (
@@ -20,9 +17,6 @@ export function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/rooms" element={<Rooms />} />
-        <Route path="/rooms/:id" element={<RoomDetails />} />
-        <Route path="/booking-success" element={<BookingSuccess />} />
       </Routes>
       <div>
         <BuscarUsuario />
